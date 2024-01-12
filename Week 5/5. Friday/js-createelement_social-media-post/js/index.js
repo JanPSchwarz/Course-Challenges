12609df42598ee549f3c01f8ecd3388218619464
Jan Schwarz <jan-paul@schw-a-rz.de>
2024-01-12 11:43:07 +0100

    week5/friday chall 2 solved

--- Week 5/5. Friday/js-createelement_social-media-post/js/index.js
+++ Week 5/5. Friday/js-createelement_social-media-post/js/index.js
@@ -10,3 +10,32 @@ likeButton.addEventListener("click", handleLikeButtonClick);
 
 // Exercise:
 // Use document.createElement() and append another social media post to the body.
+
+const article = document.createElement("article");
+const paragraph = document.createElement("p");
+const footer = document.createElement("footer");
+const span = document.createElement("span");
+const button = document.createElement("button");
+
+article.classList.add("post");
+paragraph.classList.add("post__content");
+footer.classList.add("post__footer");
+span.classList.add("post__username");
+button.classList.add("post__button");
+
+// button.setAttribute("datja-js", "like__buttonTwo");
+
+paragraph.textContent =
+  "Lorem ipsum dolor, sit amet consectetur adipisicing elit.";
+span.textContent = "@username";
+button.textContent = "♥ Like";
+
+article.append(paragraph, footer, span, button);
+
+document.body.append(article);
+
+button.addEventListener("click", handleLikeButtonClick);
+
+// const likeButtonTwo = document.querySelector('[data-js="like__buttonTwo"]');
+
+// likeButtonTwo.addEventListener("click", handleLikeButtonClick);
