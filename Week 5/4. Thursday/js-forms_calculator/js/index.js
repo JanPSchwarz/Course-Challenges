8667e4a8fdf592cb8daa23d60cac2201120ff3f7
Jan Schwarz <jan-paul@schw-a-rz.de>
2024-01-12 08:24:26 +0100

    solved chall week5/thursday

--- Week 5/4. Thursday/js-forms_calculator/js/index.js
+++ Week 5/4. Thursday/js-forms_calculator/js/index.js
@@ -3,6 +3,11 @@ console.clear();
 const form = document.querySelector('[data-js="form"]');
 const resultOutput = document.querySelector('[data-js="result"]');
 
+// const addition = document.querySelector("#addition");
+// const substraction = document.querySelector("#subtraction");
+// const multiplication = document.querySelector("#multiplication");
+// const division = document.querySelector("#division");
+
 function add(a, b) {
   return a + b;
 }
@@ -22,10 +27,28 @@ function divide(a, b) {
 form.addEventListener("submit", (event) => {
   event.preventDefault();
 
+  const formData = new FormData(event.target);
+  const data = Object.fromEntries(formData);
+
+  console.log(data);
+
   let result;
 
-  // --v-- write your code here --v--
+  const inputA = data.numberA;
+  const inputB = data.numberB;
 
+  const operator = data.operator;
+
+  // --v-- write your code here --v--
+  if (data.operator === "addition") {
+    result = add(Number(inputA), Number(inputB));
+  } else if (data.operator === "subtraction") {
+    result = subtract(Number(inputA), Number(inputB));
+  } else if (data.operator === "multiplication") {
+    result = multiply(Number(inputA), Number(inputB));
+  } else {
+    result = divide(Number(inputA), Number(inputB));
+  }
   // --^-- write your code here --^--
 
   resultOutput.textContent = result;
