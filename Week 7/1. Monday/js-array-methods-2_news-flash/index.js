fd3bff1356f2c67669025b2b6f4e88cd38970734
Jan Schwarz <jan-paul@schw-a-rz.de>
2024-01-22 14:31:44 +0100

    week7/monday all chal solved

---
+++ Week 7/1. Monday/js-array-methods-2_news-flash/index.js
@@ -0,0 +1,34 @@
+import { news } from "./utils/news.js";
+import { Card } from "./components/Card/Card.js";
+import { checkFilteredNews, checkSortedNews } from "./utils/results.js";
+
+const container = document.querySelector('[data-js="card-container"]');
+
+// Part 1 - start here
+const filteredNews = news.filter((card) =>
+  card.categories.includes("politics")
+);
+
+// Part 2 - start here
+const sortedNews = filteredNews;
+
+sortedNews.forEach((news) => {
+  const cardElement = Card(news);
+  container.append(cardElement);
+});
+
+sortedNews.sort((a, b) => {
+  if (a.body.length < b.body.length) {
+    return -1;
+  }
+  if (a.body.length > b.body.length) {
+    return 1;
+  }
+
+  return 0;
+});
+
+// Check your filter and sorting order here:
+checkFilteredNews(filteredNews);
+
+checkSortedNews(sortedNews);
