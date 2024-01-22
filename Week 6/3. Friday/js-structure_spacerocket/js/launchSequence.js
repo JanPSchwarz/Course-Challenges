41c1bb2fea9191e5eb6759d2f53ed1ca7b3a22df
Jan Schwarz <jan-paul@schw-a-rz.de>
2024-01-22 15:11:47 +0100

    week6/friday chall 3 solved

--- Week 6/3. Friday/js-structure_spacerocket/js/launchSequence.js
+++ Week 6/3. Friday/js-structure_spacerocket/js/launchSequence.js
@@ -1,5 +1,34 @@
 // Implement the launch sequence function here and export it as the default export.
 
-export default launch() {
-    
+import { loadPayload } from "./core/load.js";
+import { fuel } from "./core/fuel.js";
+import { NFSAT, FISHSAT } from "./payload/satellites.js";
+import { liftoff } from "./core/liftoff.js";
+import { deployPayload } from "./core/deploy.js";
+import { countdown } from "./core/countdown.js";
+
+export default function launch() {
+  loadPayload(NFSAT);
+  loadPayload(FISHSAT);
+
+  fuel();
+
+  for (let i = 1; i <= 5; i++) {
+    countdown();
+  }
+
+  liftoff();
+
+  deployPayload();
 }
+
+// loadPayload(NFSAT);
+// loadPayload(FISHSAT);
+
+// fuel();
+
+// for (let i = 0; i <= 0; i++) {
+//     liftoff();
+// }
+
+// deployPayload();
