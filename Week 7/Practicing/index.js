6bb4ed437840a885db76257864d0f7f46060c2c3
Jan Schwarz <jan-paul@schw-a-rz.de>
2024-01-25 12:21:01 +0100

    Chall week7/thursday + praciticing

--- Week 7/Practicing/index.js
+++ Week 7/Practicing/index.js
@@ -1,13 +1,18 @@
-async function cats() {
-  const response = await fetch(
-    "https://api.thecatapi.com/v1/images/search?limit=10&has_breeds=1&api_key=live_80QHtDPhcDJgMWfVMivtOm4RkbsEB7Op11NNA8NkImpLpcuUvYoyb12eDy5cLmnb"
-  );
-  const data = await response.json();
+// async function fetchCats() {
+//   const url =
+//     "https://api.thecatapi.com/v1/images/search?limit=10&has_breeds=1&api_key=live_80QHtDPhcDJgMWfVMivtOm4RkbsEB7Op11NNA8NkImpLpcuUvYoyb12eDy5cLmnb";
 
-  console.table(data);
-}
+//   const response = await fetch(url);
+//   const data = await response.json();
 
-cats();
+//   // const catData = data[0].breeds;
+
+//   console.log(data);
+
+//   return data;
+// }
+
+// fetchCats();
 
 function Card(cat) {
   // Create Card and Image
@@ -27,39 +32,43 @@ function Card(cat) {
   const spanOne = document.createElement("span");
 
   listItemOne.textContent = "Breed-Name:";
-  spanOne.textContent = `${cat.breed_name}`;
+  spanOne.textContent = `${cat.breeds[0].name}`;
 
   const listItemTwo = document.createElement("li");
   const spanTwo = document.createElement("span");
 
   listItemTwo.textContent = "Origin:";
-  spanTwo.textContent = `${cat.country}`;
+  spanTwo.textContent = `${cat.breeds[0].origin}`;
 
   const listItemThree = document.createElement("li");
   const spanThree = document.createElement("span");
 
   listItemThree.textContent = "Temperament:";
-  spanThree.textContent = `${cat.temperament}`;
+  spanThree.textContent = `${cat.breeds[0].temperament}`;
 
   const listItemFour = document.createElement("li");
   const spanFour = document.createElement("span");
 
   listItemFour.textContent = "Description:";
-  spanFour.textContent = `${cat.description}`;
+  spanFour.textContent = `${cat.breeds[0].description}`;
 
   const listItemFive = document.createElement("li");
   const spanFive = document.createElement("span");
   const link = document.createElement("a");
 
-  link.setAttribute("href", `${cat.wikipedia}`);
+  link.setAttribute("href", `${cat.breeds[0].wikipedia_url}`);
   link.textContent = "Wikipedia-Article";
+  link.setAttribute("target", "_blank");
 
   list.classList.add("cat-description");
+
+  const divider = document.createElement("div");
+  divider.classList.add("divider");
 
   // Append: Assemble List
 
   listItemOne.append(spanOne);
-  listItemTwo.append(listItemTwo);
+  listItemTwo.append(spanTwo);
   listItemThree.append(spanThree);
   listItemFour.append(spanFour);
   spanFive.append(link);
@@ -73,10 +82,12 @@ function Card(cat) {
     listItemFive
   );
 
-  card.append(img, list);
+  card.append(img, list, divider);
 
   return card;
 }
+
+// ** Appends Card to corresponding DIV
 
 const root = document.getElementById("start-box");
 
@@ -84,3 +95,28 @@ function renderElement(element) {
   // Append Element to the Start-Box
   root.append(element);
 }
+
+async function fetchAndRenderCats() {
+  const url =
+    "https://api.thecatapi.com/v1/images/search?limit=10&has_breeds=1&api_key=live_80QHtDPhcDJgMWfVMivtOm4RkbsEB7Op11NNA8NkImpLpcuUvYoyb12eDy5cLmnb";
+
+  const response = await fetch(url);
+  const data = await response.json();
+
+  console.log(data);
+
+  root.textContent = "";
+
+  data.forEach((cat) => {
+    const card = Card(cat);
+    renderElement(card);
+    console.log(cat);
+  });
+
+  const note = document.querySelector(".note");
+  note.removeAttribute("hidden");
+}
+
+const button = document.querySelector('[data-js="button"]');
+
+button.addEventListener("click", () => fetchAndRenderCats());
