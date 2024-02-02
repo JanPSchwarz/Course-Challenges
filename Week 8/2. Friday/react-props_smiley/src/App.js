d5d6fc507cba1b742937e2bd0003e533d0e26158
Jan Schwarz <jan-paul@schw-a-rz.de>
2024-02-02 11:49:07 +0100

    week8/friday chall 3 solved

--- Week 8/2. Friday/react-props_smiley/src/App.js
+++ Week 8/2. Friday/react-props_smiley/src/App.js
@@ -2,5 +2,15 @@ import React from "react";
 import "./styles.css";
 
 export default function App() {
-  return <h1>Replace me with your component!</h1>;
+  return (
+    <div>
+      <Smiley isHappy={false} happy="😏" sad="😭" />
+    </div>
+  );
 }
+
+function Smiley({ isHappy, happy, sad }) {
+  return <h1>{isHappy ? `${happy}` : `${sad}`}</h1>;
+}
+
+/* //return isHappy ? <h1>😏</h1> : <h1>😭</h1>; */
