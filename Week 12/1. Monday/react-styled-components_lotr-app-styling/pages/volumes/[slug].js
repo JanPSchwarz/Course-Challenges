3e7366a9fd21a8a9ac6608a8d68d136ad0f0378a
Jan Schwarz <jan-paul@schw-a-rz.de>
2024-02-26 13:05:40 +0100

    week12/monday chall solved

--- Week 12/1. Monday/react-styled-components_lotr-app-styling/pages/volumes/[slug].js
+++ Week 12/1. Monday/react-styled-components_lotr-app-styling/pages/volumes/[slug].js
@@ -2,6 +2,44 @@ import Image from "next/image";
 import Link from "next/link";
 import { useRouter } from "next/router";
 import { volumes } from "../../lib/data";
+import styled from "styled-components";
+// import LeftArrow from "../../icons/left-arrow.svg";
+
+const StyledBookSection = styled.section`
+  background: ${({ $color }) => $color};
+  /* box-shadow: var(--box-shadow-book); */
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+  padding: 20px;
+`;
+
+const StyledImage = styled(Image)`
+  box-shadow: var(--box-shadow-book);
+
+  &:hover {
+    box-shadow: var(--box-shadow-book--hover);
+  }
+`;
+
+const StyledList = styled.ul`
+  list-style: none;
+  padding: 20px;
+  color: var(--color-smoke);
+`;
+
+const StyledListItem = styled.li`
+  display: block;
+  padding: 10px;
+  color: white;
+  font-size: 0.9rem;
+`;
+
+const StyledStrong = styled.strong`
+  display: block;
+  padding: 20px 0px;
+  font-size: 1.2rem;
+`;
 
 export default function VolumeDetail() {
   const router = useRouter();
@@ -24,19 +62,21 @@ export default function VolumeDetail() {
       <Link href="/volumes">← All Volumes</Link>
       <h1>{title}</h1>
       <p>{description}</p>
-      <ul>
-        {books.map(({ ordinal, title }) => (
-          <li key={title}>
-            {ordinal}: <strong>{title}</strong>
-          </li>
-        ))}
-      </ul>
-      <Image
-        src={cover}
-        alt={`Cover image of ${title}`}
-        width={140}
-        height={230}
-      />
+      <StyledBookSection $color={volume.color}>
+        <StyledList>
+          {books.map(({ ordinal, title }) => (
+            <StyledListItem key={title}>
+              {ordinal} <StyledStrong>{title}</StyledStrong>
+            </StyledListItem>
+          ))}
+        </StyledList>
+        <StyledImage
+          src={cover}
+          alt={`Cover image of ${title}`}
+          width={140}
+          height={230}
+        />
+      </StyledBookSection>
       {previousVolume ? (
         <div>
           <Link href={`/volumes/${previousVolume.slug}`}>
--- Week 12/1. Monday/react-styled-components_lotr-app-styling/pages/volumes/index.js
+++ Week 12/1. Monday/react-styled-components_lotr-app-styling/pages/volumes/index.js
@@ -1,18 +1,66 @@
 import Link from "next/link";
 import { introduction, volumes } from "../../lib/data";
+import styled from "styled-components";
+import Image from "next/image";
+
+console.log(volumes[0].cover);
+
+const StyledList = styled.ul`
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-around;
+  list-style: none;
+  margin: 50px 0;
+`;
+
+const StyledListItem = styled.li`
+  text-decoration: none;
+  text-align: center;
+`;
+
+const StyledLink = styled(Link)`
+  display: flex;
+  flex-direction: column;
+  gap: 20px;
+  width: 100px;
+  text-decoration: none;
+  color: black;
+
+  &:hover {
+    color: green;
+  }
+`;
+
+const StyledImage = styled(Image)`
+  display: block;
+  box-shadow: var(--box-shadow-book);
+
+  &:hover {
+    box-shadow: var(--box-shadow-book--hover);
+  }
+`;
 
 export default function Volumes() {
   return (
     <>
       <h1>The Lord of the Rings</h1>
       <p>{introduction}</p>
-      <ul>
+      <h2>All Volumes</h2>
+      <StyledList>
         {volumes.map((volume) => (
-          <li key={volume.id}>
-            <Link href={`/volumes/${volume.slug}`}>{volume.title}</Link>
-          </li>
+          <StyledListItem key={volume.id}>
+            <StyledLink href={`/volumes/${volume.slug}`}>
+              <StyledImage
+                src={volume.cover}
+                alt="alt"
+                width={100}
+                height={150}
+              />
+              {volume.title}
+            </StyledLink>
+          </StyledListItem>
         ))}
-      </ul>
+      </StyledList>
     </>
   );
 }
